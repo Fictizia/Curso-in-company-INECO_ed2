0edbef4e881defa1340a79bf9b70db573c41d103
jmanchenos <45742483+jmanchenos@users.noreply.github.com>
2018-12-13 14:10:31 +0100

    Create metro.js

---
+++ jmanchenos/metro.js
@@ -0,0 +1,13 @@
+let lineas = document.getElementsByClassName("list__lineas__element")
+for(let i=0;i<lineas.length;i++){
+    let elemNombre = lineas[i].querySelector('a img');
+    let nombre = elemNombre?elemNombre.className:'';
+    let elemEstado = lineas[i].querySelector('span.state--green');
+    let elemAlerta = lineas[i].querySelector('span span.state--alert');
+    let estado= elemEstado?'normal':'parada';
+    let alerta = '';
+    if (elemEstado && elemAlerta){
+        alerta=document.getElementById(elemAlerta.parentNode.parentNode.getAttribute('data-toggle')).title;
+    }  
+    console.log(`Circulacion ${estado} en ${nombre} ${alerta}`);  
+}
